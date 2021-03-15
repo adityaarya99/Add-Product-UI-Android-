dd8e500ebf487dd60775fa52b53ef0117b232399
adityaarya99 <aryaadityacr7@gmail.com>
2021-03-15 16:19:42 +0530

    Added state to the Gender Buttons

--- Test/app/src/main/java/com/example/test/MainActivity.java
+++ Test/app/src/main/java/com/example/test/MainActivity.java
@@ -30,28 +30,126 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener ,View.OnClickListener{
     ImageView viewImage;
-    Button b;
-    Button imageButton;
+
+    Button  imageButtonSelector,imageButtonSelector2,imageButtonSelector3,btnSelectPhoto;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        spinnerCategory();
+        spinnerBrand();
         addListenerOnButton();
+        spinnerSize();
 
+        spinnerCondition();
+        imageButtonSelector = (Button) findViewById(R.id.imageButtonSelector);
 
-        b = (Button) findViewById(R.id.btnSelectPhoto);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                selectImage();
-            }
-        });
+        btnSelectPhoto = (Button) findViewById(R.id.btnSelectPhoto);
+        imageButtonSelector2 = (Button) findViewById(R.id.imageButtonSelector2);
+        imageButtonSelector3 = (Button) findViewById(R.id.imageButtonSelector3);
+        btnSelectPhoto.setOnClickListener(this);
+        imageButtonSelector.setOnClickListener(this);
+        imageButtonSelector2.setOnClickListener(this);
+        imageButtonSelector3.setOnClickListener(this);
     }
+
+    private void spinnerCondition() {
+        Spinner spinner4 = (Spinner) findViewById(R.id.spinner4);
+        spinner4.setOnItemSelectedListener(this);
+        List<String> categories4 = new ArrayList<String>();
+        categories4.add(getString(R.string.condition));
+        categories4.add("Business Services");
+        categories4.add("Computers");
+        categories4.add("Education");
+        categories4.add("Personal");
+        categories4.add("Travel");
+
+        // Creating adapter for spinnerbasic.xml
+        ArrayAdapter<String> dataAdapter4 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories4);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter4.setDropDownViewResource(R.layout.spinnerbasic);
+
+        // attaching data adapter to spinnerbasic.xml
+        spinner4.setAdapter(dataAdapter4);
+
+    }
+
+    private void spinnerBrand() {
+        Spinner spinner3 = (Spinner) findViewById(R.id.spinner3);
+        spinner3.setOnItemSelectedListener(this);
+
+        List<String> categories3 = new ArrayList<String>();
+        categories3.add("Brand  (optional)");
+        categories3.add("Business Services");
+        categories3.add("Computers");
+        categories3.add("Education");
+        categories3.add("Personal");
+        categories3.add("Travel");
+
+        // Creating adapter for spinnerbasic.xml
+        ArrayAdapter<String> dataAdapter3 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories3);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter3.setDropDownViewResource(R.layout.spinnerbasic);
+
+        // attaching data adapter to spinnerbasic.xml
+        spinner3.setAdapter(dataAdapter3);
+    }
+
+    private void spinnerCategory() {
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+
+        List<String> categories = new ArrayList<String>();
+        categories.add("Category (required)");
+        categories.add("Business Services");
+        categories.add("Computers");
+        categories.add("Education");
+        categories.add("Personal");
+        categories.add("Travel");
+
+        // Creating adapter for spinnerbasic.xml
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(R.layout.spinnerbasic);
+
+        // attaching data adapter to spinnerbasic.xml
+        spinner.setAdapter(dataAdapter);
+
+    }
+
+
+
+    private void spinnerSize() {
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+        spinner2.setOnItemSelectedListener(this);
+
+        List<String> categories2 = new ArrayList<String>();
+        categories2.add("Size (required)");
+        categories2.add("Business Services");
+        categories2.add("Computers");
+        categories2.add("Education");
+        categories2.add("Personal");
+        categories2.add("Travel");
+
+        // Creating adapter for spinnerbasic.xml
+        ArrayAdapter<String> dataAdapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories2);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter2.setDropDownViewResource(R.layout.spinnerbasic);
+
+        // attaching data adapter to spinnerbasic.xml
+        spinner2.setAdapter(dataAdapter2);
+    }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -138,89 +236,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         }
 
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        spinner.setOnItemSelectedListener(this);
-
-        List<String> categories = new ArrayList<String>();
-        categories.add("Category (required)");
-        categories.add("Business Services");
-        categories.add("Computers");
-        categories.add("Education");
-        categories.add("Personal");
-        categories.add("Travel");
-
-        // Creating adapter for spinnerbasic.xml
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
-
-        // Drop down layout style - list view with radio button
-        dataAdapter.setDropDownViewResource(R.layout.spinnerbasic);
-
-        // attaching data adapter to spinnerbasic.xml
-        spinner.setAdapter(dataAdapter);
-
-//Spinner
-        Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
-        spinner2.setOnItemSelectedListener(this);
-
-        List<String> categories2 = new ArrayList<String>();
-        categories2.add("Size (required)");
-        categories2.add("Business Services");
-        categories2.add("Computers");
-        categories2.add("Education");
-        categories2.add("Personal");
-        categories2.add("Travel");
-
-        // Creating adapter for spinnerbasic.xml
-        ArrayAdapter<String> dataAdapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories2);
-
-        // Drop down layout style - list view with radio button
-        dataAdapter2.setDropDownViewResource(R.layout.spinnerbasic);
-
-        // attaching data adapter to spinnerbasic.xml
-        spinner2.setAdapter(dataAdapter2);
+    }
 
 
-        //Third Spinner
-        Spinner spinner3 = (Spinner) findViewById(R.id.spinner3);
-        spinner3.setOnItemSelectedListener(this);
-
-        List<String> categories3 = new ArrayList<String>();
-        categories3.add("Brand  (optional)");
-        categories3.add("Business Services");
-        categories3.add("Computers");
-        categories3.add("Education");
-        categories3.add("Personal");
-        categories3.add("Travel");
-
-        // Creating adapter for spinnerbasic.xml
-        ArrayAdapter<String> dataAdapter3 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories3);
-
-        // Drop down layout style - list view with radio button
-        dataAdapter3.setDropDownViewResource(R.layout.spinnerbasic);
-
-        // attaching data adapter to spinnerbasic.xml
-        spinner3.setAdapter(dataAdapter3);
 
 
-        //Forth Spinner
-        Spinner spinner4 = (Spinner) findViewById(R.id.spinner4);
-        spinner4.setOnItemSelectedListener(this);
-        List<String> categories4 = new ArrayList<String>();
-        categories4.add(getString(R.string.condition));
-        categories4.add("Business Services");
-        categories4.add("Computers");
-        categories4.add("Education");
-        categories4.add("Personal");
-        categories4.add("Travel");
-
-        // Creating adapter for spinnerbasic.xml
-        ArrayAdapter<String> dataAdapter4 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories4);
-
-        // Drop down layout style - list view with radio button
-        dataAdapter4.setDropDownViewResource(R.layout.spinnerbasic);
-
-        // attaching data adapter to spinnerbasic.xml
-        spinner4.setAdapter(dataAdapter4);}
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -238,25 +258,41 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     public void addListenerOnButton() {
 
-        imageButton = (Button) findViewById(R.id.imageButtonSelector);
-        imageButton = (Button) findViewById(R.id.imageButtonSelector2);
-        imageButton = (Button) findViewById(R.id.imageButtonSelector3);
-
-        imageButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View arg0) {
-
-                Toast.makeText(MainActivity.this,
-                        "Gender Button is clicked!",
-                        Toast.LENGTH_SHORT).show();
-
-            }
-
-        });
-
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            case R.id.btnSelectPhoto:
+                selectImage();
+                break;
+            case R.id.imageButtonSelector:
+                imageButtonSelector.setBackground(getResources().getDrawable(R.drawable.red));
+                imageButtonSelector2.setBackground(getResources().getDrawable(R.drawable.grey));
+                imageButtonSelector3.setBackground(getResources().getDrawable(R.drawable.grey));
+                Toast.makeText(MainActivity.this,
+                        "Male Button is clicked!",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.imageButtonSelector2:
+                imageButtonSelector2.setBackground(getResources().getDrawable(R.drawable.red));
+                imageButtonSelector.setBackground(getResources().getDrawable(R.drawable.grey));
+                imageButtonSelector3.setBackground(getResources().getDrawable(R.drawable.grey));
+                Toast.makeText(MainActivity.this,
+                        "Female Button is clicked!",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.imageButtonSelector3:
+                imageButtonSelector3.setBackground(getResources().getDrawable(R.drawable.red));
+                imageButtonSelector.setBackground(getResources().getDrawable(R.drawable.grey));
+                imageButtonSelector2.setBackground(getResources().getDrawable(R.drawable.grey));
+                Toast.makeText(MainActivity.this,
+                        "Others Button is clicked!",
+                        Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 }
 
